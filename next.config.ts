4f730de6563e631c407e31e83e8de7dd2c66bc9b
Kdgrya <blackniga04@gmail.com>
2026-07-02 09:06:12 -0400

    cors

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
   async headers() {
     return [
       {
-        crossOrigin: 'anonymous',
+        // Matching all API routes
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, 
+          { 
+            key: 'Access-Control-Allow-Origin', 
+            // Note: If Allow-Credentials is true, some browsers/platforms 
+            // strict-check '*' and require an explicit origin instead.
+            value: '*' 
+          }, 
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
         ],
@@ -19,4 +23,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
