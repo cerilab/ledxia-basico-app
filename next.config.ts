b0a244ee88635fd48a07eabedf73e9e17c8e0ef7
Kdgrya <blackniga04@gmail.com>
2026-07-02 11:20:36 -0400

    cors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const crossOrigin = process.env.NEXT_PUBLIC_CORS_ORIGIN || "https://ledxia.com/servicio";
+const crossOrigin = process.env.NEXT_PUBLIC_CORS_ORIGIN || "https://ledxia.com/servicios";
 const nextConfig: NextConfig = {
   async headers() {
     return [
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers: [
           { key: "Access-Control-Allow-Origin", value: crossOrigin },
           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, anonymous" },
         ],
       },
     ];
